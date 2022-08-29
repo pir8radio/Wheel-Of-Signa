07ee5ba33818a0efc4b72bf80cfd64c400afcf55
pir8radio <pir8radio@gmail.com>
2022-08-29 16:57:50 -0500

    Add files via upload
    Support for liquidity pool, added LP ATM to game.

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -69,6 +69,36 @@ const scriptsInEvents = {
 					runtime.globalVars.burntTMG = burntTMGvar
 			    }
 			
+		},
+
+		async AtmEvent_Event3_Act3(runtime, localVars)
+		{
+			confirm();
+		},
+
+		async AtmEvent_Event4_Act3(runtime, localVars)
+		{
+			clearfields();
+		},
+
+		async AtmEvent_Event5_Act3(runtime, localVars)
+		{
+			withdraw(0.5);
+		},
+
+		async AtmEvent_Event6_Act3(runtime, localVars)
+		{
+			withdraw(1);
+		},
+
+		async AtmEvent_Event7_Act3(runtime, localVars)
+		{
+			withdraw(2);
+		},
+
+		async AtmEvent_Event8_Act3(runtime, localVars)
+		{
+			withdraw(5);
 		}
 
 };
